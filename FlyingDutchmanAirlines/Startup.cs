c69ea30e28d8c185049b44e7b565f488937e5334
Juan Carmona <juancece@outlook.com>
2022-08-02 18:49:43 -0500

    Second

---
+++ FlyingDutchmanAirlines/Startup.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FlyingDutchmanAirlines
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseRouting();
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+    }
+}
